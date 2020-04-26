6a672665397b63002cfd2f7f703c91be92458e03
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-04-26 18:14:31 +0200

    First commit

---
+++ routes/admin.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+const express = require('express')
+
+const rootDir = require('../helpers/path')
+
+const router = express.Router()
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+    console.log('in the next middleware')
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+})
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+module.exports = router
