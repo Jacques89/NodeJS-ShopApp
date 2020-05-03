12d2d421e8e6971d50a282f23cfa62c8c7c97822
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-05-03 23:20:09 +0200

    Refactored HTML to EJS

--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,29 @@
-const path = require('path')
+const path = require("path");
 
-const express = require('express')
+const express = require("express");
 
-const rootDir = require('../helpers/path')
+const rootDir = require("../helpers/path");
 
-const router = express.Router()
+const router = express.Router();
+
+const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-    console.log('in the next middleware')
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-})
+router.get("/add-product", (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+});
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res) => {
-    console.log(req.body)
-    res.redirect('/')
-})
+router.post("/add-product", (req, res) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
+});
 
-module.exports = router
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,20 @@
-const path = require('path')
+const path = require("path");
+const express = require("express");
+const rootDir = require("../helpers/path");
+const adminData = require("./admin");
 
-const express = require('express')
+const router = express.Router();
 
-const rootDir = require('../helpers/path')
-
-const router = express.Router()
-
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  })
 })
 
-module.exports = router 
+module.exports = router;
