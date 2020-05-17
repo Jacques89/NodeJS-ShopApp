b223fe3da254ee0e3d14275302432ee2cdd774a6
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-05-17 21:56:05 +0200

    added delete functionality

--- routes/shop.js
+++ routes/shop.js
@@ -21,4 +21,6 @@ router.get('/checkout', shopController.getCheckout)
 // POST-Requests
 router.post('/cart', shopController.postCart)
 
+router.post('/cart-delete-item', shopController.postCartDeleteProduct)
+
 module.exports = router
