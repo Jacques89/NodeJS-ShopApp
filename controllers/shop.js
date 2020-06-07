67ac6d0c258a578d73173c928ec841d8bbbbec4c
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-06-08 00:34:40 +0200

    able to reset cart and output order

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product')
-const Cart = require('../models/cart')
 
 // Products
 exports.getProducts = (req, res, next) => {
@@ -48,7 +47,7 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
+    .then((cart) => {
       return cart
         .getProducts()
         .then((products) => {
@@ -73,54 +72,93 @@ exports.postCart = (req, res, next) => {
   let newQuantity = 1
   req.user
     .getCart()
-    .then(cart => {
+    .then((cart) => {
       fetchedCart = cart
       return cart.getProducts({ where: { id: prodId } })
     })
-    .then(products => {
-      let product;
+    .then((products) => {
+      let product
       if (products.length > 0) {
         product = products[0]
       }
 
       if (product) {
-        const oldQuantity = product.cartItem.quantity;
-        newQuantity = oldQuantity + 1;
+        const oldQuantity = product.cartItem.quantity
+        newQuantity = oldQuantity + 1
         return product
       }
-      return Product.findByPk(prodId);
+      return Product.findByPk(prodId)
     })
-    .then(product => {
+    .then((product) => {
       return fetchedCart.addProduct(product, {
-        through: { quantity: newQuantity }
+        through: { quantity: newQuantity },
       })
     })
     .then(() => {
       res.redirect('/cart')
     })
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err))
 }
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId
-  Product.findByPk(prodId, (product) => {
-    Cart.deleteProduct(prodId, product.price)
-    res.redirect('/cart')
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts({ where: { id: prodId } })
+    })
+    .then(products => {
+      const product = products[0]
+      return product.cartItem.destroy()
+    })
+    .then(result => {
+      res.redirect('/cart')
+    })
+    .catch(err => console.log(err))
+}
+
+exports.postOrder = (req, res, next) => {
+  let fetchedCart
+  req.user
+  .getCart()
+  .then(cart => {
+    fetchedCart = cart
+    return cart.getProducts()
+  })
+  .then(products => {
+    return req.user.createOrder().then(order => {
+     return order.addProducts(
+        products.map(product => {
+        product.orderItem = { quantity: product.cartItem.quantity }
+        return product
+      }))
+    }).catch(err => {
+      console.log(err)
+    })
+  })
+  .then(result => {
+    fetchedCart.setProducts(null)
+  })
+  .then(result => {
+    res.redirect('/orders')
+  })
+  .catch(err => {
+    console.log(err)
   })
 }
 
 // Orders
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders',
+  req.user
+  .getOrders({include: ['products']})
+  .then(orders => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders,
+    })
   })
-}
-
-// Checkout
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout',
+  .catch(err => {
+    console.log(err)
   })
 }
--- routes/shop.js
+++ routes/shop.js
@@ -16,11 +16,11 @@ router.get('/cart',shopController.getCart)
 
 router.get('/orders',shopController.getOrders)
 
-router.get('/checkout', shopController.getCheckout)
-
 // POST-Requests
 router.post('/cart', shopController.postCart)
 
 router.post('/cart-delete-item', shopController.postCartDeleteProduct)
+
+router.post('/create-order', shopController.postOrder)
 
 module.exports = router
