e6500a99c9fc6f4cf90602a397d5a2ef7af87918
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-06-14 18:59:44 +0200

    now storing user to DB

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 
 const errorController = require('./controllers/error')
 const mongoConnect = require('./helpers/database').mongoConnect
+const User = require('./models/user')
 
 const app = express()
 
@@ -16,13 +17,12 @@ const shopRoutes = require('./routes/shop')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use((req, res, next) => {
-//   User.findByPk(1)
-//     .then((user) => {
-//       req.user = user
-//       next()
-//     })
-//     .catch((err) => console.log(err))
-  next()
+  User.findById("5ee6523919c7518224914a78")
+    .then((user) => {
+      req.user = user
+      next()
+    })
+    .catch((err) => console.log(err))
 })
 
 app.use('/admin', adminRoutes)
