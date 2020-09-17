967c8cc5b81aac9132a5a6e51afde14c515d9a73
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-09-17 16:40:07 +0200

    removed all data breaches

---
+++ controllers/auth.js
@@ -0,0 +1,121 @@
+const bcrypt = require('bcryptjs')
+const nodemailer = require('nodemailer')
+const mailgunTransporter = require('nodemailer-mailgun-transport')
+
+const User = require("../models/user")
+
+const mg = nodemailer.createTransport(mailgunTransporter({
+  auth: {
+    api_key: process.env.API_KEY,
+    domain: process.env.DOMAIN
+  }
+}))
+
+exports.getLogin = (req, res, next) => {
+  let message = req.flash('error')
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    errorMessage: message
+  })
+}
+
+exports.getSignup = (req, res, next) => {
+  let message = req.flash('error')
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    errorMessage: message
+  })
+}
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email
+  const password = req.body.password
+  User.findOne({
+    email: email
+  })
+  .then(user => {
+    if (!user) {
+      return res.redirect('/login')
+    }
+    bcrypt
+    .compare(password, user.password)
+    .then(passwordMatch => {
+      if (passwordMatch) {
+        req.session.isLoggedIn = true
+        req.session.user = user
+        return req.session.save(err => {
+          console.log(err)
+          res.redirect('/')
+        })
+      }
+      req.flash('error', 'Invalid email or password.')
+      res.redirect('/login')
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/login')
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email
+  const password = req.body.password
+  const confirmPassword = req.body.confirmPassword
+  User.findOne({
+    email: email
+  })
+  .then(userDoc => {
+    if (userDoc) {
+      req.flash('error', 'E-mail already exists')
+      return res.redirect('/signup')
+    }
+    return bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] }
+      })
+      return user.save()
+    })
+    .then(result => {
+      res.redirect('/login')
+      return mg.sendMail({
+        from: 'shop@jacques.com',
+        to: email,
+        subject: 'Signup succeeded',
+        text: 'Welcome'
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
