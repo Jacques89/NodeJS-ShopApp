e3fc8e044beee4f0867b6e6efa27d774d8cef1de
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-09-23 21:16:10 +0200

    created global error handler middleware, adjusted order of code execution in App.JS

--- app.js
+++ app.js
@@ -36,8 +36,15 @@ app.use(session({
   saveUninitialized: false,
   store: store
 }))
+
 app.use(csrfProtection)
 app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn
+  res.locals.csrfToken = req.csrfToken()
+  next()
+})
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -52,14 +59,8 @@ app.use((req, res, next) => {
     next()
   })
   .catch(err => {
-    throw new Error(err)
+    next(new Error(err))
   })
-})
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn
-  res.locals.csrfToken = req.csrfToken()
-  next()
 })
 
 app.use('/admin', adminRoutes)
@@ -71,7 +72,11 @@ app.get('/500', errorController.get500)
 app.use(errorController.get404)
 
 app.use((error, req, res, next) => {
-  res.redirect('/500')
+  res.status(500).render("500", { 
+    pageTitle: "Server Error", 
+    path: "/500", 
+    isAuthenticated: req.session.isLoggedIn 
+  })
 })
 
 mongoose
